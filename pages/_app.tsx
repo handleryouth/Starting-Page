4d1315c6e056819fb28695c53f92bad59a4cb7b0
Tony <rafaeltonydavid@yahoo.com>
2021-12-11 15:48:41 +0700

    fix: fix bug

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,6 @@
+import "../styles/globals.css";
 import type { AppProps } from "next/app";
 import "@fontsource/open-sans";
-import "../styles/globals.css";
 import { Layout } from "../components";
 
 function MyApp({ Component, pageProps }: AppProps) {
